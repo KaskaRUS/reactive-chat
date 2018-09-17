162b0e1094199511e9062d5195a4c3ff2541a419
Nikolai Zhdanov <nikolai@zhdanov.space>
2018-09-17 23:57:48 +0300

    initial spring boot

---
+++ src/main/kotlin/space/zhdanov/laboratory/reactive/chat/ChatApplication.kt
@@ -0,0 +1,11 @@
+package space.zhdanov.laboratory.reactive.chat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChatApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChatApplication>(*args)
+}
